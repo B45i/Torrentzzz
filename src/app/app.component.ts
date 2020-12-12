a44eedc8c92707d9c6831205d6556ebc750f1199
b45i <amalshajan2011@gmail.com>
2020-12-13 00:00:54 +0530

    baisc app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, ToastController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
@@ -11,20 +12,41 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent implements OnInit {
     text = '';
-    constructor(
-        private platform: Platform,
-        private splashScreen: SplashScreen,
-        private statusBar: StatusBar
-    ) {
-        this.initializeApp();
-    }
+    isLoading = false;
+    torrents: Array<any>;
 
-    initializeApp() {
-        this.platform.ready().then(() => {
-            this.statusBar.styleDefault();
-            this.splashScreen.hide();
-        });
+    constructor(
+        private http: HttpClient,
+    public toastController: ToastController
+    ) {
     }
 
     ngOnInit() {}
+
+    onSearch() {
+        this.isLoading = true;
+        this.http
+            .get(`https://thawing-hamlet-05815.herokuapp.com/search/${this.text}`)
+            .subscribe((x: any) => {
+                this.isLoading = false;
+                this.torrents = x.torrents;
+            });
+    }
+
+    copyMagnet(magnet: string) {
+        let tempInput = document.createElement("input") as any;
+        tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+        tempInput.value = magnet;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempInput);
+        
+        this.toastController
+          .create({
+            message: 'Magnet Link Copied to Clipboard',
+            duration: 2000,
+          })
+          .then((toast) => toast.present());
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -11,19 +12,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    ],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
