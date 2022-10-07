995e7b3fba8311db2d7b8c67e9051c9a02caa36f
b45i <amalshajan2011@gmail.com>
2022-10-07 21:55:50 +0530

    added API

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,33 @@
     import Search from './lib/Search.svelte';
     import Torrent from './lib/Torrent.svelte';
 
-    export const getTorrents = async searchTerm => {
+    let torrents = [];
+
+    const handleSearch = async ({ detail }) => {
+        const { searchTerm } = detail;
+        try {
+            const response = await getTorrents(searchTerm);
+            if (response.status === 200) {
+                const data = await response.json();
+                torrents = (data.torrents || []).filter(
+                    torrent => torrent.magnet
+                );
+                console.log(torrents);
+            }
+        } catch (error) {
+            // todo: proper error handling
+            console.log(error.message);
+        }
+    };
+
+    const getTorrents = async searchTerm => {
         return fetch(
             `https://thawing-hamlet-05815.herokuapp.com/search/${searchTerm}`
         );
     };
 </script>
 
-<Search />
+<Search on:search={handleSearch} />
 <main>
     <Torrent />
 </main>
--- src/lib/Torrent.svelte
+++ src/lib/Torrent.svelte
@@ -1,4 +1,5 @@
-<script lang="ts"></script>
+<script lang="ts">
+</script>
 
 <div class="torrent">
     <div class="details">
