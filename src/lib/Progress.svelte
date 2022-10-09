7491ff1ac09943e7f7532f02ba837ea32ffd80b5
Kaushik <kaushik@Kaushiks-MacBook-Pro.local>
2022-10-09 21:19:07 +0530

    fixed loader

--- src/lib/Progress.svelte
+++ src/lib/Progress.svelte
@@ -1,33 +1,35 @@
 <script lang="ts">
-    export let isLoading = true;
+  export let isLoading = false;
 </script>
 
 <div class="progress">
+  {#if isLoading}
     <div class="progress-bar" />
+  {/if}
 </div>
 
 <style>
-    .progress {
-        position: relative;
-        height: 5px;
+  .progress {
+    position: relative;
+    height: 5px;
+  }
+
+  .progress-bar {
+    height: 100%;
+    position: relative;
+    width: 60px;
+    background-color: var(--clr-green);
+    animation: 2s infinite alternate proggress;
+    animation-timing-function: linear;
+  }
+
+  @keyframes proggress {
+    from {
+      left: 0;
     }
 
-    .progress-bar {
-        height: 100%;
-        position: relative;
-        width: 60px;
-        background-color: var(--clr-green);
-        animation: 2s infinite alternate proggress;
-        animation-timing-function: linear;
+    to {
+      left: calc(100% - 60px);
     }
-
-    @keyframes proggress {
-        from {
-            left: 0;
-        }
-
-        to {
-            left: calc(100% - 60px);
-        }
-    }
+  }
 </style>
